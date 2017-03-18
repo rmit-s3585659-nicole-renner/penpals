8a5ad915fdba22faf261e778ac839335a9da7092
rmit-s3618265-purwa-gaikwad <purwagaikwad@ua-visitor-ten-21-136-66.uniaccess.unimelb.edu.au>
2017-03-18 16:27:20 +1100

    Signup page

--- app.js
+++ app.js
@@ -10,18 +10,30 @@ const config = {
     password: 'admin#ZIGMA145#',
     server: 'codebrew2017penpal.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
     database: 'dbpenpal',
-
+    tableName: 'testtable',
     options: {
         encrypt: true // Use this if you're on Windows Azure
     }
 }
-var tableName = "testtable";
 
-sql.connect(config).then(() => {
-    console.dir("I'm connected to DB now");
-    return sql.query `select * from ` + tableName;
-}).then(result => {
-    console.dir(result);
-}).catch(err => {
-    console.dir(err);
-})
+function login(user, pass){
+    sql.connect(config).then(() => {
+        console.dir("Trying to login");
+        return sql.query () ;
+    }).then(result => {
+        console.dir(result);
+    }).catch(err => {
+        console.dir(err);
+    })
+}
+
+function signup(str_user, str_pass, str_email, int_agegroup, str_country, str_lang, str_otherlang, arr_interest, arr_expertise, str_aboutme){
+    sql.connect(config).then(() => {
+        console.dir("Trying to login");
+        return sql.query () ;
+    }).then(result => {
+        console.dir(result);
+    }).catch(err => {
+        console.dir(err);
+    })
+}

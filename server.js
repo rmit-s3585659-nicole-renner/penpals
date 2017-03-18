eb99abbf30c4cbdac665278330f8e6f23c87b172
rmit-s3585659-nicole-renner <s3585659@student.rmit.edu.au>
2017-03-18 15:05:18 +1100

    Update server.js
    Shifted res.end to wrap all code.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World. It works!!!!\n');
     const opn = require('opn')
     opn('index.html')
+    res.end('Hello World. I"m alive!!!!!\n');
 }).listen(port);

fee6be9394462168abdc94824a51239b9e7941f7
sonya <s3575553@student.rmit.edu.au>
2017-03-19 12:47:35 +1100

    update version of chat page

---
+++ js/_vti_cnf/chat.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|SONYAZHAO\\sonya
+vti_modifiedby:SR|SONYAZHAO\\sonya
+vti_timelastmodified:TR|19 Mar 2017 01:07:42 -0000
+vti_timecreated:TR|18 Mar 2017 23:44:22 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|chatPage.html
+vti_nexttolasttimemodified:TW|19 Mar 2017 01:06:08 -0000
+vti_cacheddtm:TX|19 Mar 2017 01:07:42 -0000
+vti_filesize:IR|484
